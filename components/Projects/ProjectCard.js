958a72f0a1915fe44699085ad8416ebe30072bf9
Liam <liam.winterton.dev@gmail.com>
2022-04-02 21:14:19 +0100

    Added projects and projects card components

---
+++ components/Projects/ProjectCard.js
@@ -0,0 +1,24 @@
+import Button from '@/components/UI/Button'
+
+const ProjectCard = ({ title, technologies, subtitle, content }) => {
+	return (
+		<div className="bg-white p-16 rounded-lg shadow-2xl flex justify-between gap-9">
+			<div className="flex flex-col flex-grow">
+				<div className="mb-6">
+					<h3 className="text-4xl font-bold mb-3">{title}</h3>
+					<p className="text-lg">{technologies.join(' - ')}</p>
+				</div>
+				<div>
+					<p className="subtitle mb-3">{subtitle}</p>
+					<p>{content}</p>
+				</div>
+				<div className="mt-auto">
+					<Button href="/" text="View Project" />
+				</div>
+			</div>
+			<div className="w-[600px] h-[350px] bg-gray-300 rounded-lg"></div>
+		</div>
+	)
+}
+
+export default ProjectCard
---
+++ components/Projects/Projects.js
@@ -0,0 +1,50 @@
+import ProjectCard from '@/components/Projects/ProjectCard'
+
+const Projects = () => {
+	const projects = [
+		{
+			id: 1,
+			title: 'Star Wars: The Archive',
+			technologies: ['React', 'Next.js'],
+			subtitle: 'Star Wars Wiki',
+			content:
+				'A collection of Star Wars information, displayed in an easy to read format. Information provided via a public API'
+		},
+		{
+			id: 2,
+			title: 'Star Wars: The Archive',
+			technologies: ['React', 'Next.js'],
+			subtitle: 'Star Wars Wiki',
+			content:
+				'A collection of Star Wars information, displayed in an easy to read format. Information provided via a public API'
+		},
+		{
+			id: 3,
+			title: 'Star Wars: The Archive',
+			technologies: ['React', 'Next.js'],
+			subtitle: 'Star Wars Wiki',
+			content:
+				'A collection of Star Wars information, displayed in an easy to read format. Information provided via a public API'
+		}
+	]
+
+	return (
+		<div className="space-y-32">
+			{projects.map(project => {
+				const { id, title, technologies, subtitle, content } = project
+
+				return (
+					<ProjectCard
+						key={id}
+						title={title}
+						technologies={technologies}
+						subtitle={subtitle}
+						content={content}
+					/>
+				)
+			})}
+		</div>
+	)
+}
+
+export default Projects
