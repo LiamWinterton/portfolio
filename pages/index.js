7fc5a8fc5d0848793c925062d18f20315af60379
Liam <liam.winterton.dev@gmail.com>
2022-04-02 21:14:07 +0100

    Remove padding from main page container

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Hero from '@/components/Hero/Hero'
+import Projects from '@/components/Projects/Projects'
 
 export default function Home() {
 	return (
-		<article className="container">
+		<article className="container p-0">
 			<Hero />
+			<Projects />
 		</article>
 	)
 }
