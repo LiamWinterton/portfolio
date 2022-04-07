316825513e5a82efb968b3b077e962be8e8b30cf
Liam <liam.winterton.dev@gmail.com>
2022-04-07 22:55:50 +0100

    Trouble figuring out this damn absolute positioning

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -6,7 +6,7 @@ import Button from '@/components/UI/Button'
 const ContactForm = () => {
 	return (
 		<div className="bg-white py-16 mt-20 relative">
-			<div className="container w-11/12 relative z-50">
+			<div className="container w-11/12 py-16 absolute inset-0 z-50">
 				<form className="shadow-2xl rounded-lg max-w-md mx-auto my-16 bg-white p-9">
 					<legend className="text-3xl font-bold mb-9 text-center text-primary">
 						Want to connect?
@@ -28,15 +28,17 @@ const ContactForm = () => {
 					<Button type="submit" text="Send"></Button>
 				</form>
 			</div>
-			<div className="absolute z-40 top-0 right-0 w-6/12 py-16">
-				<Image
-					src="/images/lego-background.jpg"
-					layout="responsive"
-					alt=""
-					width={460}
-					height={307}
-					className="rounded-l-xl"
-				/>
+			<div className="ml-auto w-1/2 h-full">
+				<div className="relative h-full w-full">
+					<Image
+						src="/images/lego-background.jpg"
+						layout="responsive"
+						alt=""
+						width={460}
+						height={307}
+						className="rounded-l-xl h-full object-fill"
+					/>
+				</div>
 			</div>
 		</div>
 	)
