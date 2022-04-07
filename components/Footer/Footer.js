accaaa4ef4027c41c12bdf4795f24dad5d436ba5
Liam <liam.winterton.dev@gmail.com>
2022-04-07 23:00:02 +0100

    Added footer Component

---
+++ components/Footer/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+	return (
+		<footer className="py-12 bg-primary text-white">
+			<div className="container w-11/12">
+				<p>
+					Liam Winterton, Nottinghamshire,{' '}
+					<a href="mailto:liam.winterton.dev@gmail.com">
+						liam.winterton.dev@gmail.com
+					</a>
+				</p>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
--- components/UI/Layout.js
+++ components/UI/Layout.js
@@ -1,10 +1,12 @@
 import Header from '@/components/Header/Header'
+import Footer from '@/components/Footer/Footer'
 
 const Layout = ({ children }) => {
 	return (
 		<>
 			<Header />
 			{children}
+			<Footer />
 		</>
 	)
 }
