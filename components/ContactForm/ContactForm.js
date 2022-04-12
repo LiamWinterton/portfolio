e8c24b3bc3757879366f6dc9f41a14e9e445b913
Liam <liam.winterton.dev@gmail.com>
2022-04-12 12:24:19 +0100

    Fixed styling of contact form

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -6,8 +6,8 @@ import Button from '@/components/UI/Button'
 const ContactForm = () => {
 	return (
 		<div className="bg-white py-16 mt-20 relative">
-			<div className="container w-11/12 py-16 absolute inset-0 z-50">
-				<form className="shadow-2xl rounded-lg max-w-md mx-auto my-16 bg-white p-9">
+			<div className="container w-11/12 py-16 relative z-50">
+				<form className="shadow-2xl rounded-lg max-w-md mx-auto bg-white p-9">
 					<legend className="text-3xl font-bold mb-9 text-center text-primary">
 						Want to connect?
 					</legend>
@@ -24,19 +24,25 @@ const ContactForm = () => {
 							name="email"
 							id="email"
 						/>
+						<Input
+							label="Your Message"
+							type="textarea"
+							name="message"
+							id="message"
+						/>
 					</fieldset>
 					<Button type="submit" text="Send"></Button>
 				</form>
 			</div>
-			<div className="ml-auto w-1/2 h-full">
-				<div className="relative h-full w-full">
+			<div className="absolute inset-0 h-full w-full">
+				<div className="w-1/2 h-full ml-auto py-16 nextimage-full">
 					<Image
 						src="/images/lego-background.jpg"
 						layout="responsive"
 						alt=""
 						width={460}
 						height={307}
-						className="rounded-l-xl h-full object-fill"
+						className="rounded-l-xl h-full object-cover"
 					/>
 				</div>
 			</div>
--- components/UI/Input.js
+++ components/UI/Input.js
@@ -1,6 +1,22 @@
 const Input = props => {
 	const { label, type, name, id } = props
 
+	if (type === 'textarea') {
+		return (
+			<div className="flex flex-col">
+				<label className="block mb-3 font-bold" htmlFor={id}>
+					{label}
+				</label>
+				<textarea
+					className="form-input border-0 border-b-2 border-primary bg-gray-50"
+					name={name}
+					id={id}
+					rows={3}
+				/>
+			</div>
+		)
+	}
+
 	return (
 		<div className="flex flex-col">
 			<label className="block mb-3 font-bold" htmlFor={id}>
