23cc0ffb85e4868ef7c73160f4c603129727deaa
Liam <liam.winterton.dev@gmail.com>
2022-05-27 14:37:22 +0100

    Add some basic meta data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Hero from '@/components/Hero/Hero'
 import Projects from '@/components/Projects/Projects'
 import ContactForm from '@/components/ContactForm/ContactForm'
+import Head from 'next/head'
 
 export default function Home() {
 	return (
 		<article>
+			<Head>
+				<title>{'Liam.winterton | Full Stack Web Developer'}</title>
+			</Head>
 			<div className="container w-11/12">
 				<Hero />
 				<Projects />
